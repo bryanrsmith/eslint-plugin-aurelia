2af6372329945d185253244d94c9f6623f3a5080
Bryan Smith <BryanRSmith@gmail.com>
2016-02-17 23:14:45 -0800

    Add inject-type rule

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import injectRule from './rules/inject-matches-ctor';
+import injectTypeRule from './rules/inject-type';
 import noConventionsRule from './rules/no-conventions';
 import noConsoleLogRule from './rules/no-console-log';
 import storeUnsubscribeRule from './rules/store-unsubscribe';
@@ -7,6 +8,7 @@ import sortClassMembers, { defaultOrder } from './rules/sort-class-members';
 module.exports = {
 	rules: {
 		'inject-matches-ctor': injectRule,
+		'inject-type': injectTypeRule,
 		'no-conventions': noConventionsRule,
 		'no-console-log': noConsoleLogRule,
 		'store-unsubscribe': storeUnsubscribeRule,
@@ -14,6 +16,7 @@ module.exports = {
 	},
 	rulesConfig: {
 		'inject-matches-ctor': 2,
+		'inject-type': 0,
 		'no-conventions': 0,
 		'no-console-log': 0,
 		'store-unsubscribe': 0,
---
+++ src/rules/inject-type.js
@@ -0,0 +1,20 @@
+import { getInjectInfo } from '../inject-helper';
+
+export default function injectTypeRule(context) {
+	let type = context.options[0];
+
+	return {
+		'ClassDeclaration'(node) {
+			let injectInfo = getInjectInfo(node, context.getSourceCode());
+
+			if (!injectInfo.injects.length) {
+				return;
+			}
+
+			let inject = injectInfo.injects[0];
+			if (inject.type !== type) {
+				context.report(inject.node, `Expected injected dependencies to be declared with a ${type}.`);
+			}
+		},
+	};
+}

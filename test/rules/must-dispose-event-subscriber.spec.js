37163f9e48446cef99f9a843f8a0fd64b9d9ec04
twttao <ttao@thoughtworks.com>
2017-04-17 13:50:17 +0800

    Fix eslint

--- test/rules/must-dispose-event-subscriber.spec.js
+++ test/rules/must-dispose-event-subscriber.spec.js
@@ -4,38 +4,38 @@ import rule from '../../src/rules/must-dispose-event-subscriber';
 const ruleTester = new eslint.RuleTester({ parser: 'babel-eslint' });
 
 ruleTester.run('must-dispose-event-subscriber', rule, {
-  valid: [
-    {
-      code: `
-        export class AureliaComponent {
-          attached() {
-            this.myAureliaEventSubscriber = this.eventAggregator.subscribe('my-aurelia-event', () => {});
-          }
-          detached() {
-            this.myAureliaEventSubscriber.dispose();
-          }
-        }
-        `
-    }
-  ],
-  invalid: [
-    {
-      code: `
-        export class AureliaComponent {
-          attached() {
-            this.myAureliaEventSubscriber = this.eventAggregator.subscribe('my-aurelia-event', () => {});
-            this.myAureliaEventSubscriber2 = this.eventAggregator.subscribe(Events.EventGroup.MyEvent, () => {});
-          }
-        }
-      `,
-      errors: [{
-        message: `Component AureliaComponent subscribed 'my-aurelia-event' but does not dispose.`,
-        type: 'ClassDeclaration'
-      },
-      {
-        message: `Component AureliaComponent subscribed 'MyEvent' but does not dispose.`,
-        type: 'ClassDeclaration'
-      }]
-    }
-  ]
+	valid: [
+		{
+			code: `
+            export class AureliaComponent {
+                attached() {
+                    this.myAureliaEventSubscriber = this.eventAggregator.subscribe('my-aurelia-event', () => {});
+                }
+                detached() {
+                    this.myAureliaEventSubscriber.dispose();
+                }
+            }`,
+		},
+	],
+	invalid: [
+		{
+			code: `
+                export class AureliaComponent {
+                    attached() {
+                        this.myAureliaEventSubscriber = this.eventAggregator.subscribe('my-aurelia-event', () => {});
+                        this.myAureliaEventSubscriber2 = this.eventAggregator.subscribe(Events.EventGroup.MyEvent, () => {});
+                    }
+                }
+            `,
+			errors: [
+				{
+					message: 'Component AureliaComponent subscribed "my-aurelia-event" but does not dispose.',
+					type: 'ClassDeclaration',
+				},
+				{
+					message: 'Component AureliaComponent subscribed "MyEvent" but does not dispose.',
+					type: 'ClassDeclaration',
+				}],
+		},
+	],
 });

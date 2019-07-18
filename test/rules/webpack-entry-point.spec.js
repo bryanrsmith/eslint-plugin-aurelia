160583dda47cb6cf1382d8b18e689c74723c4994
Barrie Treloar <barrie@mea.com.au>
2019-07-18 10:04:23 +0930

    refactor(rule/webpack-entry-point): use embedFilenameInCode

--- test/rules/webpack-entry-point.spec.js
+++ test/rules/webpack-entry-point.spec.js
@@ -13,21 +13,29 @@ const moduleExportsEntryAppNotAurelia = `
   };
 `;
 
+const embedFilenameInCode = ({ filename, code }) => ({
+	filename,
+	code: `// filename=${filename}\n${code}`,
+});
+
 // Use https://astexplorer.net/ and `espree` tokens to transform your `code`
 // into an AST for use here.
 ruleTester.run('webpack-entry-point', rule, {
 	valid: [
 		{
-			// Only webpack.config.js is checked
-			filename: '/some/dir/not.webpack.config.js',
-			code: `// filename=/some/dir/not.webpack.config.js ${moduleExportsEntryAppNotAurelia}`,
+			// Only webpack.config.js is checked for valid entry.app values
+			...embedFilenameInCode({
+				filename: '/some/dir/not.webpack.config.js',
+				code: moduleExportsEntryAppNotAurelia,
+			}),
 		},
 	],
 	invalid: [
 		{
-			filename: '/some/dir/webpack.config.js',
-			code: `// filename=/some/dir/webpack.config.js ${moduleExportsEntryAppNotAurelia}`,
-			moduleExportsEntryAppNotAurelia,
+			...embedFilenameInCode({
+				filename: '/some/dir/webpack.config.js',
+				code: moduleExportsEntryAppNotAurelia,
+			}),
 			errors: [
 				{
 					message:

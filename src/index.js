dfb400f6bdea309a479ce105fcbb06e7d33f4029
Barrie Treloar <barrie@mea.com.au>
2019-07-22 11:59:43 +0930

    fix(rules): add new rules to src/index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import injectRule from './rules/inject-matches-ctor';
 import injectTypeRule from './rules/inject-type';
 import noConventionsRule from './rules/no-conventions';
 import noConsoleLogRule from './rules/no-console-log';
+import platformModulename from './rules/platform-modulename';
 import storeUnsubscribeRule from './rules/store-unsubscribe';
 import sortClassMembers, { defaultOrder } from './rules/sort-class-members';
+import webpackEntryPoint from './rules/webpack-entry-point';
 
 module.exports = {
 	rules: {
@@ -11,8 +13,10 @@ module.exports = {
 		'inject-type': injectTypeRule,
 		'no-conventions': noConventionsRule,
 		'no-console-log': noConsoleLogRule,
+		'platform-modulename': platformModulename,
 		'store-unsubscribe': storeUnsubscribeRule,
 		'sort-class-members': sortClassMembers,
+		'webpack-entry-point': webpackEntryPoint,
 	},
 	configs: {
 		recommended: {
@@ -21,10 +25,12 @@ module.exports = {
 				'inject-type': 0,
 				'no-conventions': 0,
 				'no-console-log': 0,
+				'platform-modulename': 0,
 				'store-unsubscribe': 2,
 				'sort-class-members': [ 2, {
 					order: defaultOrder,
 				}],
+				'webpack-entry-point': 0,
 			},
 		},
 	},
